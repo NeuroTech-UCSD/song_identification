77339bae6a7cf0a29db82700f20c8c592624b691
Aditya <adityatparmar@gmail.com>
2023-02-14 11:54:06 -0800

    added the main matlab file as a template

---
+++ scripts/main.m
@@ -0,0 +1,20 @@
+
+
+% Program constants
+datafile = 'ImageryData.mat';
+nfolds = 1;
+ntestfolds = 1;
+tmin = 1;
+tmax = 1;
+lambda = 1;
+
+% Load data
+load(datafile, 'eeg', 'stim', 'downFs');
+
+% Generate training sets
+
+% Cross validation
+
+% Train model
+
+% Generate plots
